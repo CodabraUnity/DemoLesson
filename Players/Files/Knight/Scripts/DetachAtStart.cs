bbf4f6dc06f8e5cbb2958116430770ae1d13ed46
frolushka <mpfrolovstud@gmail.com>
2017-10-21 08:02:11 +0300

    Refolder AntonAndr's files to the same name folder

---
+++ Players/Files/Knight/Scripts/DetachAtStart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetachAtStart : MonoBehaviour {
+
+	void Start()
+	{
+		gameObject.transform.parent = null;
+	}
+}
