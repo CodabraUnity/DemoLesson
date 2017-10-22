7fda0a654c610ee1c0e028204ed3a5420479c2ac
AntonAndr <andryushchenko_anton@live.ru>
2017-10-22 22:06:52 +0300

    OnGUI fix

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -6,31 +6,22 @@ namespace Codabra.Demo
     public class ChestScript : MonoBehaviour
     {
         private Animator _animator;
-        private bool _isWin = false;
+        private Text _text;
         [SerializeField]
         private string _winText = "You Win!";
-        private GUIStyle _style = new GUIStyle();
 
         private void Start()
         {
             _animator = transform.Find("Treasure_Chest_Up").GetComponent<Animator>();
-            _style.fontSize = 24;
-            _style.alignment = TextAnchor.UpperCenter;
-            _style.font = (Font)Resources.Load("Fonts/Badaboom");
-            _style.fontSize = 100;
-            _style.normal.textColor = Color.white;
+            _text = GameObject.FindGameObjectWithTag("Finish").GetComponent<Text>();
+            _text.enabled = false;
         }
 
         public void Open()
         {
-            _isWin = true;
             _animator.SetBool("Open", true);
-        }
-
-        private void OnGUI()
-        {
-            if (_isWin)
-                GUI.Label(new Rect((Screen.width) / 2 - (Screen.width) / 8, (Screen.height) / 6, (Screen.width) / 4, (Screen.height) / 4), _winText, _style);
+            _text.text = _winText;
+            _text.enabled = true;
         }
     }
 }
